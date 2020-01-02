dc7368968057eaa306d034e22e3abe9f17a5eae3
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2020-01-02 18:10:59 +0200

    Fix typo in name_project.sh

--- name_project.sh
+++ name_project.sh
@@ -1,5 +1,5 @@
 read -p 'Project slug (e.g. my_awesome_project): ' PROJECT_SLUG
-read -p 'Project slug (e.g. My Awesome Project): ' PROJECT_NAME
+read -p 'Full project name (e.g. My Awesome Project): ' PROJECT_NAME
 cat project_files.txt | xargs sed -i '' -e "s/\$PROJECT_SLUG/$PROJECT_SLUG/g"
 cat project_files.txt | xargs sed -i '' -e "s/\$PROJECT_NAME/$PROJECT_NAME/g"
 mv django_server/PROJECT_SLUG django_server/$PROJECT_SLUG
