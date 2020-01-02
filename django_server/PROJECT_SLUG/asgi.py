cfbfef390eb9ff8b190c95df4b833acb12d795a1
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2020-01-02 19:14:18 +0200

    Fix remaining references to city_logistics

--- django_server/PROJECT_SLUG/asgi.py
+++ django_server/PROJECT_SLUG/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'city_logistics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '$PROJECT_SLUG.settings')
 
 application = get_asgi_application()
